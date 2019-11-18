3cd4feb87c6484f87d12bb78177c1a2d1eccef58
JackieBinya <jacquelinebinya@gmail.com>
2019-11-18 05:22:17 +0200

    Allow a user to signup
    - installed cors module to the server and implemented it as middleware
    - refactored signUp.js as to connect it to the server thrrough fetch api
    - refactored styles.css
    [finishes #169772435]

--- UI/signUp.js
+++ UI/signUp.js
@@ -1,7 +1,65 @@
-document.querySelector('.signUp-btn').addEventListener('click', e=>{
-    window.location.href="./mentors.html";
-    /*
-    * window.location.href does not redirect buttons of type submit inside forms, it doesnt work 
-    * so for demo purposes change type to button
-    */
+const signUpForm = document.querySelector('.signUpForm');
+
+const setLoading = (bool) => {
+  const button = document.querySelector('.signUp-btn');
+  if (bool) {
+    button.disabled = true;
+    button.textContent = 'Signing up ...';
+    button.style.opacity = 0.5;
+    button.style.cursor = 'default';
+  } else {
+    button.disabled = false;
+    button.textContent = 'Sign Up';
+    button.style.opacity = 1;
+    button.style.cursor = 'pointer';
+  }
+};
+
+const showMessage = (res) => {
+  const msg = document.querySelector('.msg');
+  const displayMsg = document.querySelector('.display-msg');
+  if (res.status === 400) {
+    msg.textContent = res.error;
+    displayMsg.style.display = 'flex';
+    setLoading(false);
+  }
+
+  if (res.status === 201) {
+    signUpForm.reset();
+    localStorage.setItem('token', res.data.token);
+    window.location.href = './mentors.html';
+  }
+};
+
+const post = async (data) => {
+  try {
+    const result = await fetch('http://localhost:8001/api/v1/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    showMessage(await result.json());
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+signUpForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  setLoading(true);
+
+  post({
+    firstName: e.currentTarget.firstName.value,
+    lastName: e.currentTarget.lastName.value,
+    email: e.currentTarget.email.value,
+    password: e.currentTarget.password.value,
+  });
+});
+
+document.querySelector('.del-msg').addEventListener('click', (e) => {
+  const displayMsg = document.querySelector('.display-msg');
+  displayMsg.style.display = 'none';
 });
